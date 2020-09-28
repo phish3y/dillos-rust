580ad84f639562ab9457c5f071f9f58a18696e0d
phish3y <logsdonevan1@gmail.com>
2020-09-27 22:20:00 -0700

    display background

---
+++ README.md
@@ -0,0 +1,1 @@
+# dillos-rust
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::video::{Window, WindowContext};
+use sdl2::render::{Canvas, WindowCanvas, TextureCreator, Texture};
+use sdl2::image::{self, LoadTexture, InitFlag};
+use sdl2::rect::{Point, Rect};
+
+use std::time::Duration;
+
+fn render(canvas: &mut WindowCanvas, color: Color, background: &Texture, dillo: &Texture) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    canvas.copy(background, None, None)?;
+    canvas.copy(dillo, None, Rect::new(0, 0, 63, 85))?;
+
+    canvas.present();
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window: Window = video_subsystem.window("Dillos", 2436, 1125)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let _image_context = image::init(InitFlag::PNG)?;
+
+    let mut canvas: Canvas<Window> = window.into_canvas().build()
+        .expect("could not make a canvas");
+
+    let texture_creator: TextureCreator<WindowContext> = canvas.texture_creator();
+    let background = texture_creator.load_texture("assets/packs/pack1/level1/lv1_1.png")?;
+    let dillo = texture_creator.load_texture("assets/dillo/dwf/dwf1.png")?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut i = 0;
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
+                },
+                _ => {}
+            }
+        }
+
+        i = (i + 1) % 255;
+
+        render(&mut canvas, Color::RGB(i, 64, 255-i), &background, &dillo)?;
+
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
+}

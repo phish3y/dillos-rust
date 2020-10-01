66ff8dd7e53d39fcb4fa2094e643104bf0828054
phish3y <logsdonevan1@gmail.com>
2020-09-30 17:28:18 -0700

    add some collisions

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,46 @@ use sdl2::EventPump;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::video::{Window, WindowContext};
-use sdl2::render::{Canvas, TextureCreator};
+use sdl2::render::{Canvas, TextureCreator, Texture};
 use sdl2::image::{self, LoadTexture, InitFlag};
 use sdl2::rect::Rect;
+use sdl2::pixels::Color;
 
 use std::time::Duration;
 
 struct Dillo {
-  position: Rect
+  position: Rect,
+  x_speed: i32,
+  y_speed: i32
 }
 
 impl Dillo {
   fn new(x: i32, y: i32) -> Dillo {
     Dillo {
-      position: Rect::from((x, y, 63, 85))
+      position: Rect::from((x, y, 63, 85)),
+      x_speed: 0,
+      y_speed: 0,
     }
   }
 
-  fn update_position(& mut self, x: i32, y: i32) {
-      self.position.set_x(self.position.x + x);
-      self.position.set_y(self.position.y + y);
+  fn update_position(& mut self) {
+      self.position.set_x(self.position.x + self.x_speed);
+      self.position.set_y(self.position.y + self.y_speed);
   }
+}
+
+struct Ground {
+    rect: Rect,
+    intersect: Option<Rect>
+}
+
+impl Ground {
+    fn new(x: i32, y: i32, width: u32, height: u32) -> Ground {
+        Ground {
+            rect: Rect::new(x, y, width, height),
+            intersect: None
+        }
+    }
 }
 
 fn main() -> Result<(), String> {
@@ -40,15 +59,16 @@ fn main() -> Result<(), String> {
         .expect("could not make a canvas");
 
     let texture_creator: TextureCreator<WindowContext> = canvas.texture_creator();
-    let dillo_texture = texture_creator.load_texture("assets/dillo/dwf/dwf1.png")?;
+    let dillo_texture: Texture = texture_creator.load_texture("assets/dillo/dwf/dwf1.png")?;
     let mut dillo1 = Dillo::new(0, 0);
 
-    // TODO
-    // let g1 = Rect::from((x, y, width, height): (i32, i32, u32, u32))
+    // x, y, width, height
+    let mut g1 = Ground::new(0, 700, 650, 200);
 
     let mut event_pump: EventPump = sdl_context.event_pump()?;
 
     let mut start_game: bool = false;
+    let mut start_speed_set: bool = false;
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -64,13 +84,48 @@ fn main() -> Result<(), String> {
             }
         }
 
-        canvas.clear();
 
         if start_game {
-            dillo1.update_position(0, 5);
+            if !start_speed_set {
+                dillo1.y_speed = 5;
+                dillo1.x_speed = 3;
+                start_speed_set = true;
+            }
+
+            dillo1.update_position(); 
+
+            let intersection: Option<Rect> = g1.rect.intersection(dillo1.position);
+            if let Some(current_intersect) = intersection {
+                if let Some(previous_intersect) = g1.intersect {
+                    // there's a previous intersect, see if the y or x has changed
+                    // if height changed, set y_speed to 0, if width changed, reverse direction
+                    if current_intersect.height() != previous_intersect.height() {
+                        // we must be colliding on the y axis
+                        dillo1.y_speed = 0
+                    }
+    
+                } else {
+                    // no previous intersect, just updated the intersect on the ground object
+                    g1.intersect = intersection;
+                }
+                
+    
+            } else {
+                // not intersecting anything, so fall
+                dillo1.y_speed = 5;
+            }
         }
+        
+       
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
+        canvas.draw_rect(g1.rect)?;
 
         canvas.copy(&dillo_texture, None, dillo1.position)?;
+
 
         canvas.present();
 
